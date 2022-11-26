dcb9b67e4d9c175d009683698a5b95c9427623b9
Yash Joshi <yashjoshi2201@gmail.com>
2022-11-26 12:05:41 +0530

    Top Navigation Loader And Evn Changes

--- src/App.js
+++ src/App.js
@@ -8,23 +8,39 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
   pageSize = '6';
+  apiKey = process.env.REACT_APP_NEWS_API;
+  
+  state = {
+    progress:0
+  };
+
+  setProgress = (progress) => {
+    this.setState({progress:progress});
+  }
+
   render() {
+    console.log("Key :- ",this.apiKey,process.env.REACT_APP_NEWS_API);
     return (
       <div>
         <Router>
+          <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}    
+      />
         <NavBar/>
           <Routes>
-            <Route exact path="/" element={<News key="" pageSize={this.pageSize} country="in" category="" />}  />
-            <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business" />}  />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />}  />
-            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />}  />
-            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health" />}  />
-            <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science" />}  />
-            <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" />}  />
-            <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" />}  />
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="" pageSize={this.pageSize} country="in" category="" />}  />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="business" pageSize={this.pageSize} country="in" category="business" />}  />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />}  />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={this.pageSize} country="in" category="general" />}  />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={this.pageSize} country="in" category="health" />}  />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={this.pageSize} country="in" category="science" />}  />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports" pageSize={this.pageSize} country="in" category="sports" />}  />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology" pageSize={this.pageSize} country="in" category="technology" />}  />
           </Routes>
         </Router>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -29,22 +29,40 @@ export class News extends Component {
   }
 
   fetchMoreData = async () => {
-    this.setState({ page: this.state.page +  1});
-    const url = `https://newsapi.org/v2/top-headlines?&category=${this.props.category}&country=${this.props.country}&apiKey=82e9fff0f8884d06a7277f48ec808e3d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parseData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parseData.articles),
-      loading:false,
-      totalResults: parseData.totalResults
-    });
+    if(this.Ismounted)
+    {
+      this.props.setProgress(0);
+      this.setState({ page: this.state.page +  1});
+      const url = `https://newsapi.org/v2/top-headlines?&category=${this.props.category}&country=${this.props.country}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      this.props.setProgress(30);
+      let parseData = await data.json();
+      this.props.setProgress(70);
+      this.setState({
+        articles: this.state.articles.concat(parseData.articles),
+        loading:false,
+        totalResults: parseData.totalResults
+      });
+      this.props.setProgress(100);
+    }
+    else
+    {
+      this.setState({ page: this.state.page +  1});
+      const url = `https://newsapi.org/v2/top-headlines?&category=${this.props.category}&country=${this.props.country}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+      this.setState({
+        articles: this.state.articles.concat(parseData.articles),
+        loading:false,
+        totalResults: parseData.totalResults
+      });
+    }
   };
 
   componentDidMount() {
     if(this.Ismounted)
     {
       this.fetchMoreData();
-      console.log("page :- ",this.state.page);
       this.Ismounted = false;
     }
   }
@@ -52,14 +70,14 @@ export class News extends Component {
   render() {
     return (
       <>
-        <h2 className="text-center">News Hunter  - Top HeadLines </h2>
+        <h2 className="text-center my-4">News Hunter  - Top HeadLines </h2>
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
           hasMore={this.state.articles.length !== this.state.totalResults}
           loader={<Spinner/>}
         >
-          <div className="container">
+          <div className="container my-2">
             <div className="row">
               {this.state.articles.map((element)=>{
                   return  <div className="col-md-4 my-2" key={element.url}>
